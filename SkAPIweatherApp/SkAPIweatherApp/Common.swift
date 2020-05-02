db3a225c84140083093bdfff3a323d2c99ebecba
김광준 <kwangjun3952@gmail.com>
2020-05-02 23:01:09 +0900

    feat: added SK weather planet AppKey

---
+++ SkAPIweatherApp/SkAPIweatherApp/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  SkAPIweatherApp
+//
+//  Created by 김광준 on 2020/05/02.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import Foundation
+
+let appKey = "l7xxafb172cc733645ab8427ba01143100b3"
