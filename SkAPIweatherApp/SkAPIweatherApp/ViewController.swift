91f7a90a5a4f9456102b1959a6a0a7e432b8ca5a
김광준 <kwangjun3952@gmail.com>
2020-05-05 22:00:19 +0900

    feat: tableView datasource

--- SkAPIweatherApp/SkAPIweatherApp/ViewController.swift
+++ SkAPIweatherApp/SkAPIweatherApp/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var listTableView: UITableView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,3 +22,35 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 0
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath) as? SummaryTableViewCell else {
+                return UITableViewCell()
+            }
+            // code
+            return cell
+        }
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ForecastTableViewCell.identifier, for: indexPath) as? ForecastTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+}
